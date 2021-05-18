f3f00b368d033f71248c082b0775baaff0d6354e
max-lutz <max.lutz@edu.ece.fr>
2021-05-18 23:28:19 +0200

    apply preprocessing to df (scaling and encoding) dynamically

--- apps/classification.py
+++ apps/classification.py
@@ -1,13 +1,31 @@
 import streamlit as st
 import pandas as pd
-import base64
-import matplotlib
-import matplotlib.pyplot as plt
-import seaborn as sns
 import numpy as np
 import os
 from matplotlib.backends.backend_agg import RendererAgg
-from datetime import date
+
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import RobustScaler
+
+from sklearn.pipeline import make_pipeline
+from sklearn.compose import make_column_transformer 
+from sklearn.preprocessing import FunctionTransformer
+from sklearn.pipeline import Pipeline
+
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import RidgeClassifier
   
 
 
@@ -15,7 +33,53 @@ from datetime import date
 @st.cache
 def get_data_classification():
     df = pd.read_csv(os.path.join(os.getcwd(), 'data', 'heart_statlog.csv'))
+    df.loc[df['chest pain type'] == 1, 'chest pain type'] = 'typical angina'
+    df.loc[df['chest pain type'] == 2, 'chest pain type'] = 'atypical angina'
+    df.loc[df['chest pain type'] == 3, 'chest pain type'] = 'non-anginal pain'
+    df.loc[df['chest pain type'] == 4, 'chest pain type'] = 'asymptomatic'
+    df['chest pain type'] = df['chest pain type'].astype(str)
+
+    df.loc[df['sex'] == 1, 'sex'] = 'male'
+    df.loc[df['sex'] == 0, 'sex'] = 'female'
+    df['sex'] = df['sex'].astype(str)
+
+    df.loc[df['resting ecg'] == 0, 'resting ecg'] = 'normal'
+    df.loc[df['resting ecg'] == 1, 'resting ecg'] = 'ST-T wave abnormality'
+    df.loc[df['resting ecg'] == 2, 'resting ecg'] = 'probable or definite left ventricular hypertrophy'
+    df['resting ecg'] = df['resting ecg'].astype(str)
+
+    df.loc[df['exercise angina'] == 0, 'exercise angina'] = 'no'
+    df.loc[df['exercise angina'] == 1, 'exercise angina'] = 'yes'
+    df['exercise angina'] = df['exercise angina'].astype(str)
+
+    df.loc[df['ST slope'] == 0, 'ST slope'] = 'unsloping'
+    df.loc[df['ST slope'] == 1, 'ST slope'] = 'flat'
+    df.loc[df['ST slope'] == 2, 'ST slope'] = 'downslopping'
+    df['ST slope'] = df['ST slope'].astype(str)
+
     return df
+
+def get_encoding(encoder):
+    if encoder == 'None':
+        return 'drop'
+    if encoder == 'Ordinal encoder':
+        return OrdinalEncoder()
+    if encoder == 'OneHotEncoder':
+        return OneHotEncoder()
+
+def get_scaling(scaler):
+    if scaler == 'None':
+        return 'passthrough'
+    if scaler == 'Standard scaler':
+        return StandardScaler()
+    if scaler == 'MinMax scaler':
+        return MinMaxScaler()
+    if scaler == 'Robust scaler':
+        return RobustScaler()
+
+def get_ml_algorithm(algorithm, hyperparameters):
+    if algorithm == 'Logistic regression':
+        return 
 
 #def app():
     #configuration of the page
@@ -27,18 +91,31 @@ SPACER = .2
 ROW = 1
 
 df_classification = get_data_classification()
+X = df_classification.drop(columns = 'target')
+Y = df_classification['target'].values.ravel()
+
+passthrough_cols = ['age', 'resting bp s', 'cholesterol', 'fasting blood sugar', 'max heart rate', 'oldpeak']
+cat_cols = ['resting ecg', 'exercise angina', 'ST slope', 'chest pain type', 'sex']
+
 
 # Sidebar 
 #selection box for the different features
 st.sidebar.header('Preprocessing')
+encoder_selected = st.sidebar.selectbox('Encoding', ['None', 'Ordinal encoder', 'OneHotEncoder'], 
+                                            help='Encoding allow ML algorithms to understand categorical features.')
 
-classifier_selected = st.sidebar.selectbox('Scaling', ['None', 'Standard scaler', 'MinMax scaler', 'Robust scaler'], 
+scaler_selected = st.sidebar.selectbox('Scaling', ['None', 'Standard scaler', 'MinMax scaler', 'Robust scaler'], 
                                             help='Scaling data can improve the performance of ML algorithms.')
 
+st.sidebar.header('K fold cross validation selection')
+nb_splits = st.sidebar.slider('Number of splits', min_value=3, max_value=20)
+rdm_state = st.sidebar.slider('Random state', min_value=0, max_value=42)
 
 st.sidebar.header('Model selection')
-classifier_list = ['Logistic regression', 'Support vector', 'Kernel support', 'K neirest neighbors', 'Naive bayes', 'Decision tree', 'Random forest']
-classifier_selected = st.sidebar.selectbox('Select regression algorithm', classifier_list)
+classifier_list = ['Logistic regression', 'Support vector', 'Kernel support', 'K nearest neighbors', 'Naive bayes', 'Decision tree', 'Random forest']
+classifier_selected = st.sidebar.selectbox('', classifier_list)
+
+st.sidebar.header('Hyperparameters selection')
 
 title_spacer1, title, title_spacer_2 = st.beta_columns((.1,ROW,.1))
 with title:
@@ -52,6 +129,28 @@ with title:
             * The code can be accessed at [code](https://github.com/max-lutz/ML-exploration-tool).
             """)
 
-st.write(df_classification)
 
-st.write(df_classification.corr())
+#get_encoding(encoder_selected)
+
+preprocessing = make_column_transformer(
+    (get_encoding(encoder_selected) , cat_cols),
+    (get_scaling(scaler_selected) , passthrough_cols)
+)
+
+folds = KFold(n_splits=nb_splits, shuffle=True, random_state=rdm_state)
+
+pipeline = Pipeline([
+    ('preprocessing' , preprocessing),
+    ('ml', )
+])
+
+st.write(X)
+
+
+preprocessing.fit(X)
+X_preprocessed = preprocessing.transform(X)
+
+st.write(X_preprocessed)
+
+
+#st.write(df_classification.corr())
